9c75ce8ea5ab9b1213236a2807f80abe6c452ef4
B_Shashidhar <45161832+BShashidhar@users.noreply.github.com>
2020-01-27 23:37:42 +0530

    commit

--- controller/asset.js
+++ controller/asset.js
@@ -86,15 +86,6 @@ module.exports = {
             })
     },
 
-    // getAssetById: (req, res) => {
-    //     Asset.findOne({ where: { id: req.query.id } })
-    //         .then(result => {
-    //             res.status(200).json({ result: result })
-    //         })
-    //         .catch(err => {
-    //             res.status(500).json({ result: err.message })
-    //         })
-    // },
     getAssetById: async (req, res) => {
         var asset_id = req.body.asset_id;
         var projectName = null;
