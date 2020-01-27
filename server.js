7d08c54466fb5be2373182429ae3cf35dd4afb13
B_Shashidhar <45161832+BShashidhar@users.noreply.github.com>
2020-01-28 02:22:57 +0530

    commit

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
 const apiRouter = require('./router')
-
 const app = express()
 const PORT= 2020
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
-
 app.use('/api', apiRouter)
-
 app.use(function (err, req, res, next) {
     console.error("Error := ",err.stack)
     res.status(500).send('Server Error. Try again..!')
 })
-
 app.listen(PORT, (err) => {/*  */
     if(err)
     console.error('Unable start server : ',err)
